6a04beac7c4716e2c529ca79c41517014dc1f0c5
Mehul <Mehul@LAPTOP-60JDU1U8>
2019-11-07 09:30:39 +0530

    Initial Commit Created New Project for datastructure and algorithm Recursion

---
+++ datastructure-algorithm/src/main/java/com/mehul/algorithm/Recursion.java
@@ -0,0 +1,20 @@
+package com.mehul.algorithm;
+
+/**
+ * @author Mehul
+ *
+ */
+public class Recursion {
+
+	public static void main(String[] args) {
+		Recursion recursion = new Recursion();
+		System.out.println(recursion.fibonacci(35));
+	}
+
+	private int fibonacci(int n) {
+		if(n == 1 || n == 2) {
+			return 1;
+		}
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
+}
