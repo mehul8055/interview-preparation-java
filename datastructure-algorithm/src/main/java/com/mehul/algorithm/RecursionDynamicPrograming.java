2a279be2312fa68ffadd2edcbda7b492b17ca925
Mehul <Mehul@LAPTOP-60JDU1U8>
2019-11-07 09:43:29 +0530

    Recursion with Dynamic Programing

---
+++ datastructure-algorithm/src/main/java/com/mehul/algorithm/RecursionDynamicPrograming.java
@@ -0,0 +1,25 @@
+package com.mehul.algorithm;
+
+/**
+ * @author Mehul
+ *
+ */
+public class RecursionDynamicPrograming {
+	public static void main(String[] args) {
+		RecursionDynamicPrograming recursion = new RecursionDynamicPrograming();
+		Long[] memoize = new Long[10000];
+		System.out.println(recursion.fibonacci(35, memoize));
+	}
+
+	private Long fibonacci(int n, Long[] memoize) {
+		if(memoize[n] != null) {
+			return memoize[n];
+		}
+		if(n == 1 || n == 2) {
+			return 1l;
+		}
+		Long result = fibonacci(n-1, memoize) + fibonacci(n-2, memoize);
+		memoize[n] = result;
+		return result;
+	}
+}
