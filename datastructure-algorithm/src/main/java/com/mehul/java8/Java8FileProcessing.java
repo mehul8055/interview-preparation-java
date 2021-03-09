92a05fef25df94703c885f33951508b391cf99d4
Mehul Lakhatariya <mehul8055@gmail.com>
2021-03-09 13:53:51 +0530

    2021 : Added word count using java 8 and longest line in file

--- datastructure-algorithm/src/main/java/com/mehul/java8/Java8FileProcessing.java
+++ datastructure-algorithm/src/main/java/com/mehul/java8/Java8FileProcessing.java
@@ -5,7 +5,12 @@ import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.IntSummaryStatistics;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -15,7 +20,9 @@ import java.util.stream.Stream;
 public class Java8FileProcessing {
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
-		processEmployee();
+		//processEmployee();
+		// countWord();
+		findLongestLine();
 	}
 
 	public static void processEmployee() throws IOException, URISyntaxException {
@@ -38,5 +45,57 @@ public class Java8FileProcessing {
 		System.out.println(summaryStatistics);
 
 		employees.close();
+	}
+
+	public static void countWord() throws IOException, URISyntaxException {
+		// Create Stream of line from file wordcount.txt located in src/main/resources
+		Stream<String> lines = Files.lines(
+				Paths.get(ClassLoader.getSystemResource("wordcount.txt").toURI()),
+				StandardCharsets.UTF_8);
+		
+		Map<String, Integer> wordCountMap 
+		= lines
+			.map(line -> line.split("\\s+"))
+			.flatMap(Arrays::stream)
+			.collect(Collectors.toMap(word -> word, word -> 1, Integer::sum));
+
+		lines.close();
+
+		System.out.println(wordCountMap);
+	}
+
+	public static void findLongestLine() throws IOException, URISyntaxException {
+		class Line{
+			String line;
+			int length;
+			/**
+			 * @param line
+			 * @param length
+			 */
+			public Line(String line, int length) {
+				this.line = line;
+				this.length = length;
+			}
+			
+			public int getLength() {
+				return length;
+			}
+			
+			public String getLine() {
+				return line;
+			}
+		}
+		
+		// Create Stream of line from file wordcount.txt located in src/main/resources
+		Stream<String> lines = Files.lines(
+				Paths.get(ClassLoader.getSystemResource("wordcount.txt").toURI()),
+				StandardCharsets.UTF_8);
+		
+		Optional<Line> longestLine = lines
+			.map(line -> new Line(line, line.length()))
+			.max(Comparator.comparingInt(line -> line.getLength()));
+
+		System.out.println(longestLine.get().getLine());
+		lines.close();
 	}
 }
