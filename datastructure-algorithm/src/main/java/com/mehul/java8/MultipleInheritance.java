c0637bd0b19d1bbd7bda201f657759d94cd90931
Mehul Lakhatariya <mehul8055@gmail.com>
2021-03-13 16:25:58 +0530

    2021 : Added Multiple Inheritance example

---
+++ datastructure-algorithm/src/main/java/com/mehul/java8/MultipleInheritance.java
@@ -0,0 +1,86 @@
+package com.mehul.java8;
+
+/**
+ * This will demonstrated multiple inheritance
+ * 
+ * @author Mehul
+ *
+ */
+public class MultipleInheritance implements First, Second, Third {
+
+	@Override
+	public void print() {
+		System.out.println("Printing...");
+	}
+
+	@Override
+	public void print(int value) {
+		System.out.println("Printing Value.." + value);
+	}
+
+	public static void main(String[] args) {
+		First first = new MultipleInheritance();
+		first.print();
+		first.defaultMathodFirst();
+
+		Second second = new MultipleInheritance();
+		second.print();
+		Second.staticMethodSecond();
+		second.defaultMathodSecond();
+		printUsingLambda(() -> System.out.println("Printing from Lambda.."));
+
+	}
+
+	static void printUsingLambda(Second second) {
+		second.print();
+	}
+}
+
+/**
+ * 
+ * @author Mehul
+ *
+ */
+//This is not a functional interface
+interface First {
+
+	void print();
+
+	void print(int value);
+
+	default void defaultMathodFirst() {
+		System.out.println("Printing from default method of First.");
+	}
+
+	static void staticMethodSecond() {
+		System.out.println("Printing from static method of Second.");
+	}
+}
+
+/**
+ * 
+ * @author Mehul
+ *
+ */
+@FunctionalInterface
+interface Second extends Third {
+
+	void print();
+
+	boolean equals(Object obj);
+
+	default void defaultMathodSecond() {
+		System.out.println("Printing from default method of Second.");
+	}
+
+	static void staticMethodSecond() {
+		System.out.println("Printing from static method of Second.");
+	}
+}
+
+@FunctionalInterface
+interface Third {
+
+	void print();
+
+}
